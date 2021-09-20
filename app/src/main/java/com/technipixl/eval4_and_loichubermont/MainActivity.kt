716ee891cb54bdf2329cd2759c8cbee7178ad61a
loich91 <loichubermont@yahoo.fr>
2021-09-20 11:01:20 +0200

    third commit

--- app/src/main/java/com/technipixl/eval4_and_loichubermont/MainActivity.kt
+++ app/src/main/java/com/technipixl/eval4_and_loichubermont/MainActivity.kt
@@ -2,10 +2,13 @@ package com.technipixl.eval4_and_loichubermont
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.technipixl.eval4_and_loichubermont.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
